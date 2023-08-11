8bb6fda64ad717a52892d8087843da8bdb6bdfc5
MUKESH KUMAR <mukeshsharmabh501@gmail.com>
2023-08-11 11:40:58 +0530

    feat_match

--- img_util.py
+++ img_util.py
@@ -2,10 +2,13 @@
 
 import matplotlib.pyplot as plt
 
-def imshow(im, title,type = '' ):
+def imshow(im, title = '', type = '', cv=True ):
     if type:
         plt.imshow(im, cmap= type)
     else:
-        plt.imshow(im)
+        if cv:
+            plt.imshow(im[:,:,::-1])
+        else:
+            plt.imshow(im)
     plt.title(title)
     plt.axis('off')    
